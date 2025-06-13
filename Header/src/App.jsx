94b3cca824e21c1e70d472171b30023a89ea763c
kritika <kritikabhardwaj2513@gmail.com>
2025-06-13 23:22:51 +0530

    learnrouting

--- Header/src/App.jsx
+++ Header/src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link, useParams } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Link, useParams, Outlet } from "react-router-dom"
 
 
 function Home() {
@@ -10,48 +10,85 @@ function Home() {
 }
 function About() {
   return (
-    <h2>About Page</h2>
+    <div>
+      <Outlet />
+      <h2>About Page</h2>
+    </div>
+  )
+}
+
+
+function Team() {
+  return (
+    <h2>Team Page</h2>
+  )
+}
+function History() {
+  return (
+    <h2>History Page</h2>
+  )
+}
+
+
+
+function UserProfile() {
+  const { userId } = useParams();
+  return (
+    <h2>User Profile of {userId}</h2>
   )
 }
 
 // function UserProfile(){
-//   const {userId} = useParams();
-//   return(
-//     <h2>User Profile of {userId}</h2>
-//   )
-// }
-
-function UserProfile(){
-    const {userId} = useParams();
-    return (
-        <h2>User Profile of {userId}</h2>
-    )
-}
-
-
-// function Navigation(){
+//     const {userId} = useParams();
 //     return (
-//         <nav>
-//             <ul>
-//                 <li><Link to="/">Home</Link></li>
-//                 <li><Link to="/about">About</Link></li>
-//             </ul>
-//         </nav>
+//         <h2>User Profile of {userId}</h2>
 //     )
 // }
 
 
-export default function App(){
-    return (
-        <Router>
-            {/* <Navigation /> */}
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/about" element={<About />}/>
-                {/* <Route path="/user/:userId" element={<UserProfile/>}/> */}
-                <Route path="/user/:userId" element={<UserProfile />}/>
-            </Routes>
-        </Router>
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  )
+}
 
-    )
+function Dashboard() {
+  return (
+    <h2>Dashboard</h2>
+  )
+}
+
+function ProtectedRoute({ children, isLoggedIn }) {
+  return (
+    isLoggedIn ? children : <h2>please log in </h2>
+  )
+}
+
+
+export default function App() {
+  const isLoggedIn = true;
+
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path='team' element={<Team />} />
+          <Route path='history' element={<History />} />
+        </Route>
+        <Route path="/user/:userId" element={<UserProfile />} />
+        <Route path="/dashboard" element=
+          {<ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Dashboard />
+          </ProtectedRoute>} />
+      </Routes>
+    </Router>
+
+  )
 }
