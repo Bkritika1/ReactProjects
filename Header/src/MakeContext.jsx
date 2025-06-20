632c0cce0b600d9e788aed0fad43622900024722
kritika <kritikabhardwaj2513@gmail.com>
2025-06-20 23:48:15 +0530

    'makectxt'

--- Header/src/MakeContext.jsx
+++ Header/src/MakeContext.jsx
@@ -1,5 +1,8 @@
-export default function makeContext(){
-    return(
-        <h3>vukj</h3>
+export default function makeContext() {
+    return (
+        <>
+            <h3>vukj</h3>
+            <h5>hhjj</h5>
+        </>
     )
 }
