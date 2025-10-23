ee2d8d5f8b6f7567750a1f5015733501152791ad
kritika <kritikabhardwaj2513@gmail.com>
2025-10-23 20:25:30 +0530

    commit

--- Gradienty/src/components/Footer.jsx
+++ Gradienty/src/components/Footer.jsx
@@ -3,6 +3,8 @@ export default function Footer({ theme }) {
     <footer style={{ backgroundColor: theme?.primary || "#222", color: "#fff", textAlign: "center", padding: "2rem 1rem", marginTop: "2rem" }}>
       <p>© 2025 MyBrand. All rights reserved.</p>
       <p>© 2025 MyBrand. All rights reserved.</p>
+      <p>© 2025 MyBrand. All rights reserved.</p>
+
 
     </footer>
   );
